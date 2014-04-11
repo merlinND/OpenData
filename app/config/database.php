797e0236fbae05fa7a5bbc9f3c9b8b31393f32f4
etiennebatise <etienne.batise@gmail.com>
2014-04-11 21:44:33 +0200

    Settings of the database config file : soft, username, host, database name, and password

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'opendata',
+			'username'  => 'opendata76',
+			'password'  => 'opendata',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
