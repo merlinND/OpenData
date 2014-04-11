d6ccb0db248e01ed24b8e5d900fd81c70a7cfaa7
Antoine Augusti <antoine@augusti.fr>
2014-04-11 23:55:06 +0200

    Translation for page title and page description

--- app/lang/fr/home.php
+++ app/lang/fr/home.php
@@ -4,4 +4,8 @@ return array(
 	'twoHours' => '2 heures',
 	'afternoon' => 'Une après-midi',
 	'day' => 'Une journée',
+	// TODO
+	'pageTitle' => 'Tarte Tatin',
+	// TODO
+	'pageDescription' => 'Page description'
 );
--- app/views/homepage/header.blade.php
+++ app/views/homepage/header.blade.php
@@ -2,9 +2,9 @@
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
-	<title>{{ isset($pageTitle) ? $pageTitle : 'My Amazing Page' }}</title>
+	<title>@lang('home.pageTitle')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="{{ isset($pageDescription) ? $pageDescription : 'My amazing decription' }}">
+	<meta name="description" content="@lang('home.pageDescription')">
 
 	{{ HTML::style('assets/bootstrap/css/bootstrap.min.css'); }}
 	{{ HTML::style('assets/css/screen.css'); }}
