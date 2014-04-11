2330d854f389f59bcb926160d27bb1f925ea7d40
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-04-12 01:40:56 +0200

    API: exclude places from the results

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -11,10 +11,6 @@ class ApiController extends Controller {
 		'limit'
 	);
 
-	public static function helloApi() {
-		return "Hello API.";
-	}
-
 	public static function getRandomPlace() {
 		return Place::all()->random();
 	}
@@ -22,7 +18,7 @@ class ApiController extends Controller {
 	/**
 	 * @param from Associative array {'lat'=>, 'long'=>}
 	 * @param limit
-	 * @return A list of the <limit> closest places to <from>
+	 * @return Eloquent\Collection of the <limit> closest places to <from>
 	 */
 	public static function getAllPlaces($from, $limit = self::MAX_RESULTS) {
 		return Place::all()->slice(0, $limit);
@@ -31,11 +27,13 @@ class ApiController extends Controller {
 	/**
 	 * @param from Associative array {'lat'=>, 'long'=>}
 	 * @param filters Associative array : key = name of the filter
+	 * @return Eloquent\Collection of the filtered places
 	 */
 	public static function getPlaces($from, $filters) {
 		$candidates = self::getAllPlaces($from);
 
 		// The 'limit' filter must be applied last
+		// because we want to return the most relevant results
 		$limit = -1;
 		if (isset($filters['limit'])) {
 			$limit = $filters['limit'];
@@ -58,6 +56,7 @@ class ApiController extends Controller {
 	/**
 	 * Apply the filter corresponding to the <key>
 	 * and return the filtered set
+	 * @param $data Eloquent\Collection
 	 */
 	protected static function filter($key, $value, $data) {
 		switch ($key) {
@@ -66,7 +65,7 @@ class ApiController extends Controller {
 			case 'distance':
 				return self::filterWithDistance($value, $data);
 			case 'except':
-				return self::filterWithDistance($value, $data);
+				return self::excludePlaces($value, $data);
 			case 'limit':
 				return self::limitResults($value, $data);
 			default:
@@ -76,6 +75,7 @@ class ApiController extends Controller {
 
 	/**
 	 * Filter depending on euclidian distance from <from>
+	 * @param $candidates Eloquent\Collection
 	 */
 	protected static function filterWithDistance($params, $candidates) {
 		// TODO
@@ -85,6 +85,7 @@ class ApiController extends Controller {
 	/**
 	 * Filter depending on time_limit attribute
 	 * but also computing the needed travel time
+	 * @param $candidates Eloquent\Collection
 	 */
 	protected static function filterWithTime($timeLimit, $candidates) {
 		// TODO: first compute rough travel time based on euclidian distance and constant travel speed, then call the travel API only whith what's left
@@ -92,11 +93,18 @@ class ApiController extends Controller {
 	}
 
 	/**
-	 * @param placesId A list of the places to be excluded
+	 * @param placesId String of format [id1, id2, id3, ...]
+	 * @param $candidates Eloquent\Collection
 	 */
 	protected static function excludePlaces($placesId, $candidates) {
-		// TODO
-		return $candidates;
+
+		$placesId = json_decode($placesId);
+
+		$excluder = function($candidate) use ($placesId) {
+			return !in_array($candidate->id, $placesId);
+		};
+
+		return $candidates->filter($excluder);
 	}
 
 	/**
