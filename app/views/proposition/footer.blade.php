a40986f7c1bead7fe854c95d6114f504f304d64a
Antoine Augusti <antoine@augusti.fr>
2014-04-12 10:59:36 +0200

    Logos in the footer

--- app/views/proposition/footer.blade.php
+++ app/views/proposition/footer.blade.php
@@ -1,3 +1,13 @@
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-6">
+				{{{HTML::image('assets/img/logoOpendata.png', 'Logo Open Data', 'img-responsive')}}}
+			</div>
+
+			<div class="col-xs-6">
+				{{{HTML::image('assets/img/logoSeineMaritime.png', 'Logo Open Data', 'img-responsive')}}}
+			</div>
+		</div>
 	</div><!-- .container-fluid -->
 	</body>
 </html>
