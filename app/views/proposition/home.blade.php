ca1b2f70922bdf9c6fbc7edfe278fea2ccd556b5
Antoine Augusti <antoine@augusti.fr>
2014-04-12 02:06:55 +0200

    Views for the proposition's page

---
+++ app/views/proposition/home.blade.php
@@ -0,0 +1,7 @@
+@include('proposition/header')
+<div class="photo" style="background-image:url({{{ $backgroundURL}}})">
+	<div class="container-fluid">
+
+	</div>
+</div>
+@include('proposition/footer')
