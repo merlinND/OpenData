8e9182297becf3ee58f40e0ab1d8ddeaed21f908
Antoine Augusti <antoine@augusti.fr>
2014-04-12 08:46:55 +0200

    Adding CatchphraseSeeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('TypeTableSeeder');
 		$this->call('OpenStreetMapSeeder');
 		$this->call('ScoreSeeder');
+		$this->call('CatchphraseSeeder');
 	}
 
 }
