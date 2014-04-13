9848ec8e02a940361ea86f7aacccf674957e466f
Merlin Nimier-David <merlin.nimier@gmail.com>
2014-04-13 13:08:01 +0200

    Default catchphrase should be empty

--- app/lang/fr/catchphrases.php
+++ app/lang/fr/catchphrases.php
@@ -1,17 +1,18 @@
 <?php
 return array(
-	'typeMuseum1' => "Le musée :name, le plus beau musée de :city",
-	'typeMuseum2' => "Le musée :name, à visiter en famille",
-
-	'typeArtwork1' => "Une oeuvre d'art dans :city ?",
-
-	'typeViewpoint1' => "Envie d'un beau panorama ?",
-	'typeViewpoint1' => "Un beau panorama de :city",
-
-	'typeThemePark1' => "Il faut s'amuser au parc :name",
-
-	'typeMediatheque1' => "La médiathèque,un espace de silence pour apprendre",
-
+	'default'           => '',
+	'typeMuseum1'       => "Le musée :name, le plus beau musée de :city",
+	'typeMuseum2'       => "Le musée :name, à visiter en famille",
+	
+	'typeArtwork1'      => "Une oeuvre d'art dans :city ?",
+	
+	'typeViewpoint1'    => "Envie d'un beau panorama ?",
+	'typeViewpoint1'    => "Un beau panorama de :city",
+	
+	'typeThemePark1'    => "Il faut s'amuser au parc :name",
+	
+	'typeMediatheque1'  => "La médiathèque,un espace de silence pour apprendre",
+	
 	'typeLieuCulturel1' => "Plein de lieux culturels à côté de chez vous",
 	'typeLieuCulturel2' => "La musique et le théâtre, une tradition de :city",
 );
